79d55a5550d41b0e720cde6ba6785938cbeb75b4
arpit-pathak <blogarpit21@gmail.com>
2025-04-30 22:24:31 +0530

    added backend link in frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./components/UserList";
 import AddUser from "./components/AddUser";
 import EditUser from "./components/EditUser";
- 
+
 function App() {
   return (
     <BrowserRouter>
--- Frontend/src/components/UserList.js
+++ Frontend/src/components/UserList.js
@@ -10,13 +10,13 @@ const UserList = () => {
   }, []);
  
   const getUsers = async () => {
-    const response = await axios.get("http://localhost:5000/users");
+    const response = await axios.get(`${process.env.REACT_APP_Backend_URL}/users`);
     setUser(response.data);
   };
  
   const deleteUser = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/users/${id}`);
+      await axios.delete(`${process.env.REACT_APP_Backend_URL}/users/${id}`);
       getUsers();
     } catch (error) {
       console.log(error);
